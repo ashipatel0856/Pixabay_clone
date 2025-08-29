e2edb9f2b837fa9827d2e18a02f7f6c95e39e8c6
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-08-29 20:38:01 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import PixabayContext from '../context/PixabayContext'
 
 const Navbar = () => {
+    
+    const { fetchImageByCategory ,setQuery } = useContext(PixabayContext)
   return (
+    <>
     <div className ='container text-center my-3' >
-<button type="button" className="btn btn-outline-primary mx-3" >Nature</button>
-<button type="button" className="btn btn-outline-warning mx-3">Science</button>
-<button type="button" className="btn btn-outline-light mx-3">Computer</button>
-<button type="button" className="btn btn-outline-danger mx-3">Flowers</button>
-<button type="button" className="btn btn-outline-info mx-3">Buildings</button>
-<button type="button" className="btn btn-outline-info mx-3">Sports</button>
-<button type="button" className="btn btn-outline-light mx-3">tranports</button>
-<button type="button" className="btn btn-outline-warning mx-3">Travel</button>
-<button type="button" className="btn btn-outline-primary mx-3">Food</button>
+<button onClick ={() => fetchImageByCategory("nature")} type="button" className="btn btn-outline-primary mx-3" >Nature</button>
+<button onClick ={() => fetchImageByCategory("science")} type="button" className="btn btn-outline-warning mx-3">science</button>
+<button onClick ={() => fetchImageByCategory("computer")} type="button" className="btn btn-outline-light mx-3">Computer</button>
+<button onClick ={() => fetchImageByCategory("Flowers")} type="button" className="btn btn-outline-danger mx-3">Flowers</button>
+<button onClick ={() => fetchImageByCategory("animals")}  type="button" className="btn btn-outline-info mx-3">Animals</button>
+<button onClick ={() => fetchImageByCategory("sports")} type="button" className="btn btn-outline-info mx-3">Sports</button>
+<button onClick ={() => fetchImageByCategory("transportation")} type="button" className="btn btn-outline-light mx-3">tranports</button>
+<button onClick ={() => fetchImageByCategory("travel")} type="button" className="btn btn-outline-warning mx-3">Travel</button>
+<button onClick ={() => fetchImageByCategory("food")}  type="button" className="btn btn-outline-primary mx-3">Food</button>
 
     </div>
+    <div className="container" style={{width:'600px'}}>
+    <input type="text" onChange = {(e) =>setQuery(e.target.value)} className="form-control bg-dark text-light"   ></input>
+    </div>
+        </>
+
   )
 }
 
--- src/context/PixabayState.jsx
+++ src/context/PixabayState.jsx
@@ -3,10 +3,13 @@ import PixabayContext from './PixabayContext'
 
 const PixabayState = (props) => {
     const [imageData , setImageData] = useState([])
+
+    const [query,setQuery] = useState('london')
+    
     const api_key = "52028077-bc540d523621f7120df24dae3";
     useEffect(() => {
         const fetchData = async () => {
-            const api = await fetch(`https://pixabay.com/api/?key=${api_key}&q=london+flowers&image_type=photo&pretty=true&per_page=100`
+            const api = await fetch(`https://pixabay.com/api/?key=${api_key}&q=${query}+flowers&image_type=photo&pretty=true&per_page=100`
 
             );
             const data = await api.json();
@@ -17,10 +20,19 @@ const PixabayState = (props) => {
         fetchData();
 
 
-    }, []);
+    }, [query]);
+
+    const fetchImageByCategory = async (cat) => {
+         const api = await fetch(`https://pixabay.com/api/?key=${api_key}&category=${cat}+flowers&image_type=photo&pretty=true&per_page=100`
+
+            );
+            const data = await api.json();
+            setImageData(data.hits)
+            console.log(data.hits);
+    }
 
     return (
-        <PixabayContext.Provider value={{ imageData }} >{props.children}</PixabayContext.Provider>
+        <PixabayContext.Provider value={{ imageData, fetchImageByCategory,setQuery}} >{props.children}</PixabayContext.Provider>
     )
 }
 
